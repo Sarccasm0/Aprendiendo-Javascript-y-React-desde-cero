eb75bba31eaf049e00fc16ac33c226810561b413
Sarccasm0 <jeffers1917@hotmail.com>
2023-02-26 16:40:27 -0500

    Hola, first commit from vs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const Hola = (params) => {
+  console.log(params);
+  return <p>{params.Msg}</p>
+}
+
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hola Msg='Hola como estamos' />
+      <Hola Msg='Aqui aprendiendo React con @MiduDev'/>
+      <Hola Msg='Aprendiendo bastante sobre React. 😎'/>
     </div>
   );
 }
